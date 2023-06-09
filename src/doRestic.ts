506357b192ed4af02716e07774d614cf19159f17
RuiNtD <RuiNtD@outlook.com>
2023-06-09 12:57:07 -0600

    Update doResetic to use Deno.Command

--- src/doRestic.ts
+++ src/doRestic.ts
@@ -1,11 +1,9 @@
 import "./env.ts";
 
 export default async function doRestic(args: string[]) {
-  const p = Deno.run({
-    cmd: ["restic"].concat(args),
-  });
-  const status = await p.status();
-  return status.code;
+  const p = new Deno.Command("restic", { args });
+  const { code } = await p.output();
+  return code;
 }
 
 if (import.meta.main) Deno.exit(await doRestic(Deno.args));
