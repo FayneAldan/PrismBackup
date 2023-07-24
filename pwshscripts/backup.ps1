81d3f1f8b9c3afd273fd342b87d07598ab27eb97
RuiNtD <RuiNtD@outlook.com>
2023-07-24 01:17:17 -0600

    Fix exit codes in PowerShell scripts

--- pwshscripts/backup.ps1
+++ pwshscripts/backup.ps1
@@ -1,2 +1,3 @@
 #!/usr/bin/env pwsh
 deno run -Ar https://RuiNtD.github.io/PrismBackup/src/backup.ts $args
+exit $LASTEXITCODE
--- pwshscripts/doRestic.ps1
+++ pwshscripts/doRestic.ps1
@@ -1,2 +1,3 @@
 #!/usr/bin/env pwsh
 deno run -Ar https://RuiNtD.github.io/PrismBackup/src/doRestic.ts $args
+exit $LASTEXITCODE
